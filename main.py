c55ad3ec1f702bb7700f9190d6a079cfc19c32e2
Roman01xZ <bomjik69@mail.ru>
2024-08-20 14:30:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print (9**0.5*5) #1st program
+print (9.99>9.98 and 1000 != 1000.1) #2nd program
+print (2*2+2) #3rd program
+print (2*(2+2))
+print (2*2+2==2*(2+2))
+print ((float('123.456')) *10)#4th program"
+print ((int (1234.56)) %10)
